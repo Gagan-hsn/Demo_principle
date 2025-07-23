515e384f551ad55fba88ffae37a9d1e6eed7fcfa
Gagan-hsn <gaganshettyhsn@gmail.com>
2025-07-23 16:47:29 +0530

    Add project files.

---
+++ Demo_principle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_principal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("interest calculation");
+            Console.WriteLine("enter principal amt");
+            int principal = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter rate of interest");
+            int rate = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter time ");
+            int time = Convert.ToInt32(Console.ReadLine());
+            double interest = principal * rate * time / 100;
+            Console.WriteLine($"the interest is {interest}");
+        }
+    }
+}
